7e07715f398bff9dd7adef67b58db7ecd92fc0eb
kiliankaraus <kilian.karaus@hotmail.de>
2021-05-19 18:03:19 +0200

    Create a new React App

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,3 +1,7 @@
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
