497e273d7ece17cbe5bc3a3f3e421f180a819613
kiliankaraus <kilian.karaus@hotmail.de>
2021-05-20 10:02:34 +0200

    Create a react test app

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,15 @@
-import Amplify from "aws-amplify";
-import awsExports from "./aws-exports";
-Amplify.configure(awsExports);
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
+
 
 ReactDOM.render(
   <React.StrictMode>
