17c7342fd877e093fed88e3fbd2c358d36ef81cd
kiliankaraus <kilian.karaus@hotmail.de>
2021-05-23 18:34:48 +0200

    Add Mitglieder

--- src/App.js
+++ src/App.js
@@ -1,75 +1,85 @@
 /* src/App.js */
 import React, { useEffect, useState } from 'react'
 import Amplify, { API, graphqlOperation } from 'aws-amplify'
-import { createTodo } from './graphql/mutations'
-import { listTodos } from './graphql/queries'
+//import { createTodo } from './graphql/mutations'
+//import { listTodos } from './graphql/queries'
+
+import { DataStore } from '@aws-amplify/datastore';
+import { Mitglieder } from './models';
 
 import awsExports from "./aws-exports";
-import { withAuthenticator } from '@aws-amplify/ui-react'
+//import { withAuthenticator } from '@aws-amplify/ui-react'
 Amplify.configure(awsExports);
 
 
-const initialState = { name: '', description: '' }
+//const initialState = { name: '', description: '' }
 
-const App = () => {
-  const [formState, setFormState] = useState(initialState)
-  const [todos, setTodos] = useState([])
+await DataStore.save(
+    new Mitglieder({
+      "Name": "Lorem ipsum dolor sit amet",
+      "Alter": 1020
+    })
+);
 
-  useEffect(() => {
-    fetchTodos()
-  }, [])
-
-  function setInput(key, value) {
-    setFormState({ ...formState, [key]: value })
-  }
-
-  async function fetchTodos() {
-    try {
-      const todoData = await API.graphql(graphqlOperation(listTodos))
-      const todos = todoData.data.listTodos.items
-      setTodos(todos)
-    } catch (err) { console.log('error fetching todos') }
-  }
-
-  async function addTodo() {
-    try {
-      if (!formState.name || !formState.description) return
-      const todo = { ...formState }
-      setTodos([...todos, todo])
-      setFormState(initialState)
-      await API.graphql(graphqlOperation(createTodo, {input: todo}))
-    } catch (err) {
-      console.log('error creating todo:', err)
-    }
-  }
-
-  return (
-      <div style={styles.container}>
-        <h2>Amplify Todos</h2>
-        <input
-            onChange={event => setInput('name', event.target.value)}
-            style={styles.input}
-            value={formState.name}
-            placeholder="Name"
-        />
-        <input
-            onChange={event => setInput('description', event.target.value)}
-            style={styles.input}
-            value={formState.description}
-            placeholder="Description"
-        />
-        <button style={styles.button} onClick={addTodo}>Create Todo</button>
-        {
-          todos.map((todo, index) => (
-              <div key={todo.id ? todo.id : index} style={styles.todo}>
-                <p style={styles.todoName}>{todo.name}</p>
-                <p style={styles.todoDescription}>{todo.description}</p>
-              </div>
-          ))
-        }
-      </div>
-  )
-}
+// const App = () => {
+//   const [formState, setFormState] = useState(initialState)
+//   const [todos, setTodos] = useState([])
+//
+//   useEffect(() => {
+//     fetchTodos()
+//   }, [])
+//
+//   function setInput(key, value) {
+//     setFormState({ ...formState, [key]: value })
+//   }
+//
+//   async function fetchTodos() {
+//     try {
+//       const todoData = await API.graphql(graphqlOperation(listTodos))
+//       const todos = todoData.data.listTodos.items
+//       setTodos(todos)
+//     } catch (err) { console.log('error fetching todos') }
+//   }
+//
+//   async function addTodo() {
+//     try {
+//       if (!formState.name || !formState.description) return
+//       const todo = { ...formState }
+//       setTodos([...todos, todo])
+//       setFormState(initialState)
+//       await API.graphql(graphqlOperation(createTodo, {input: todo}))
+//     } catch (err) {
+//       console.log('error creating todo:', err)
+//     }
+//   }
+//
+//   return (
+//       <div style={styles.container}>
+//         <h2>Amplify Todos</h2>
+//         <input
+//             onChange={event => setInput('name', event.target.value)}
+//             style={styles.input}
+//             value={formState.name}
+//             placeholder="Name"
+//         />
+//         <input
+//             onChange={event => setInput('description', event.target.value)}
+//             style={styles.input}
+//             value={formState.description}
+//             placeholder="Description"
+//         />
+//         <button style={styles.button} onClick={addTodo}>Create Todo</button>
+//         {
+//           todos.map((todo, index) => (
+//               <div key={todo.id ? todo.id : index} style={styles.todo}>
+//                 <p style={styles.todoName}>{todo.name}</p>
+//                 <p style={styles.todoDescription}>{todo.description}</p>
+//               </div>
+//           ))
+//         }
+//       </div>
+//   )
+// }
 
 const styles = {
   container: { width: 400, margin: '0 auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: 20 },
@@ -80,5 +90,5 @@ const styles = {
   button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' }
 }
 
-//export default App
-export default withAuthenticator(App)
+export default App
+//export default withAuthenticator(App)
