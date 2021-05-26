b44bbf0d79500dad3603c51018f19511cc17651c
kiliankaraus <kilian.karaus@hotmail.de>
2021-05-26 11:54:57 +0200

    Add auth

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 /* src/App.js */
 import logo from './logo.svg';
 import './App.css';
-/*import React, { useEffect, useState } from 'react'
-import Amplify, { API, graphqlOperation } from 'aws-amplify'
-//import { createTodo } from './graphql/mutations'
-//import { listTodos } from './graphql/queries'
+import React from 'react'
+import awsconfig from './aws-exports'
+import Amplify from 'aws-amplify'
+import {AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react'
 
-import awsExports from "./aws-exports";
-import { withAuthenticator } from '@aws-amplify/ui-react'
-Amplify.configure(awsExports);*/
+Amplify.configure(awsconfig);
 
 
 
@@ -17,22 +15,12 @@ function App() {
   return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <AmplifySignOut/>
+            <h2>Ground Passer Bitches</h2>
         </header>
       </div>
   );
 }
 
-export default App;
-//export default withAuthenticator(App)
+
+export default withAuthenticator(App)
