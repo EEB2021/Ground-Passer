f17c3f337757f78b71907a6b15defd22034cecca
christophkuepper <kuepper.christoph@gmx.de>
2021-06-01 16:12:07 +0200

    update layout and add table component which is commented out

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,32 @@ import {AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react'
 import {listScoredbs} from "./graphql/queries";
 import{useState} from "react";
 import{useEffect} from "react";
+import { makeStyles } from '@material-ui/core/styles';
 import { DataGrid } from '@material-ui/data-grid';
 import {Paper} from '@material-ui/core';
+import {Table} from 'table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 
 Amplify.configure(awsconfig);
 
+function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+
+}
+
 function App() {
     const [scores, setScores] = useState([]);
+
+    const useStyles = makeStyles({
+        table: {
+            minWidth: 650,
+        },
+    });
+    const classes= useStyles();
 
     useEffect(() => {
         fetchScores()
@@ -30,6 +49,25 @@ function App() {
         }
     }
 
+    const columns =[
+        {field: 'id', headername: 'ID'},
+        {field: 'name', headername: 'Name'},
+        {field: 'zeit', headername: 'Zeit'},
+    ]
+    const rows =[
+
+        {name: 'Chris', zeit: '01:00', anztreffer: 9, anzfehler: 1, anzpunkte: 200},
+        {name: 'Tim', zeit: '01:00', anztreffer: 9, anzfehler: 1, anzpunkte: 200},
+        {name: 'Max', zeit: '01:00', anztreffer: 9, anzfehler: 1, anzpunkte: 200},
+        {name: 'Tom', zeit: '01:00', anztreffer: 9, anzfehler: 1, anzpunkte: 200},
+    ]
+    const rows2 = [
+        createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+        createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+        createData('Eclair', 262, 16.0, 24, 6.0),
+        createData('Cupcake', 305, 3.7, 67, 4.3),
+        createData('Gingerbread', 356, 16.0, 49, 3.9),
+    ];
 
   return (
       <div className="App">
@@ -42,16 +80,60 @@ function App() {
                   return <Paper variant ="outlined" elevation={2}>
                         <div className="scoreCard">
                             <div>
-                                <div className="name"> {scores.name} Zeit: {scores.zeit} Anzahl Treffer: {scores.anztreffer} Anzahl Fehler: {scores.anzfehler} Anzahl Punkte: {scores.punkte}</div>
+                                <div className="name"> {scores.name} </div>
+                            </div>
+                            <div>
+                                <div className="name"> {scores.zeit} </div>
+                            </div>
+                            <div>
+                                <div className="name"> {scores.anztreffer} </div>
+                            </div>
+                            <div>
+                                <div className="name"> {scores.anzfehler} </div>
+                            </div>
+                            <div>
+                                <div className="name"> {scores.punkte} </div>
                             </div>
                         </div>
                   </Paper>
               })}
           </div>
+          {/*<<div className="scoreList">
+              <TableContainer component={Paper}>
+                  <Table stickyHeader aria-label="sticky table">
+                      <TableHead>
+                          <TableRow>
+                              <TableCell>Name</TableCell>
+                              <TableCell align="right">Zeit</TableCell>
+                              <TableCell align="right">Anzahl Treffer</TableCell>
+                              <TableCell align="right">Anzahl Fehler</TableCell>
+                              <TableCell align="right">Punkte</TableCell>
+                          </TableRow>
+                      </TableHead>
+                      <TableBody>
+                          {rows.map((row)=>(
+
+                              <TableRow key={row.name}>
+                                  <TableCell component="th" scope="row">
+                                      {row.name}
+                                  </TableCell>
+                                  <TableCell align="right">{row.zeit}</TableCell>
+                                  <TableCell align="right">{row.anztreffer}</TableCell>
+                                  <TableCell align="right">{row.anzfehler}</TableCell>
+                                  <TableCell align="right">{row.anzpunkte}</TableCell>
+                              </TableRow>
+                          ))}
+                      </TableBody>
+                  </Table>
+              </TableContainer>
+          </div>*/}
+
 
       </div>
+
   );
 }
 
 
 export default withAuthenticator(App)
+
